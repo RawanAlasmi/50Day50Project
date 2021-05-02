995c2390c8deea09d191079a6d3abf6577c184bc
Rawan 7rb <rawan7rb@Rawans-MacBook-Pro.local>
2021-05-02 12:02:45 +0300

    day12

--- Day12/script.js
+++ Day12/script.js
@@ -1,36 +1,7 @@
-const jokeEl = document.getElementById('joke')
-const jokeBtn = document.getElementById('jokeBtn')
+const toggles = document.querySelectorAll('.faq-toggle')
 
-jokeBtn.addEventListener('click', generateJoke)
-
-generateJoke()
-
-// USING ASYNC/AWAIT
-async function generateJoke() {
-  const config = {
-    headers: {
-      Accept: 'application/json',
-    },
-  }
-
-  const res = await fetch('https://icanhazdadjoke.com', config)
-
-  const data = await res.json()
-
-  jokeEl.innerHTML = data.joke
-}
-
-// USING .then()
-// function generateJoke() {
-//   const config = {
-//     headers: {
-//       Accept: 'application/json',
-//     },
-//   }
-
-//   fetch('https://icanhazdadjoke.com', config)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       jokeEl.innerHTML = data.joke
-//     })
-// }
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
